8aeb46e945dadf8a22d756ea0962d30c0faae02e
Abhijeet <thezerocoder@gmail.com>
2022-02-10 16:04:55 +0530

    added comment

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+# abhijeet demo
 class StreamPlatform(models.Model):
     name=models.CharField(max_length=30)
     about=models.CharField(max_length=150)
